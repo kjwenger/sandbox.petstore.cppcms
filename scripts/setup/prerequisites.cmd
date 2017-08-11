c4e4b65bb9263bf8cb5f7389aba6112f2a8064c5
Klaus Wenger <kjwenger@yahoo.com>
2017-08-11 20:38:12 +0200

    initial load

--- scripts/setup/prerequisites.cmd
+++ scripts/setup/prerequisites.cmd
@@ -1,12 +1,15 @@
+@rem Get general tools
+choco install unzip -y
+
 @rem Get network tools
-choco install curl
-choco install wget
+choco install curl -y
+choco install wget -y
 
 @rem Get version control tools
-choco install git
-choco install subversion
+choco install git -y
+choco install subversion -y
 
 @rem Get development tools
-choco install mingw
-choco install cmake
-choco install cppcheck
+choco install mingw -y
+choco install cmake -y
+choco install cppcheck -y
