1cd2093517704db00f5a3988347cc26d5809e304
Klaus Wenger <kjwenger@yahoo.com>
2017-08-12 22:29:32 +0200

    added database

--- scripts/setup/databases.sh
+++ scripts/setup/databases.sh
@@ -6,7 +6,35 @@ SCRIPTS_DIR="$(dirname "${SETUP_DIR}")"
 PROJECT_DIR="$(dirname "${SCRIPTS_DIR}")"
 
 # Create databases
-touch "${PROJECT_DIR}/petstore_dev.db"
-touch "${PROJECT_DIR}/petstore_test.db"
-touch "${PROJECT_DIR}/petstore_prod.db"
-
+pushd "${PROJECT_DIR}"
+touch petstore_dev.db
+touch petstore_test.db
+touch petstore_prod.db
+if [ ! -f sqitch.conf ]; then
+    sqitch init "sandbox.cppcms" \
+           --uri https://github.com/theory/sqitch-sqlite-intro/ \
+           --engine sqlite
+    sqitch config --user engine.sqlite.client /usr/local/bin/sqlite3
+    sqitch config --user user.name "Klaus Wenger"
+    sqitch config --user user.email "kjwenger@yahoo.com"
+    sqitch add category -n "Creates table to track our category."
+    sqitch add tag -n "Creates table to track our tag."
+    sqitch add user -n "Creates table to track our user."
+    sqitch add order -n "Creates table to track our order."
+    sqitch add pet -n "Creates table to track our pet." \
+                   --requires category \
+                   --requires tag
+fi
+sqitch target add petstore_dev db:sqlite:petstore_dev.db
+sqitch engine add sqlite petstore_dev
+sqitch deploy
+sqitch deploy db:sqlite:petstore_test.db
+sqitch deploy db:sqlite:petstore_prod.db
+sqlite3 petstore_test.db '.tables'
+sqitch verify
+sqitch verify db:sqlite:petstore_test.db
+sqitch verify db:sqlite:petstore_prod.db
+sqitch status
+sqitch status db:sqlite:petstore_test.db
+sqitch status db:sqlite:petstore_prod.db
+popd
